e2dc1ea1084b7ae43b94ae76892c37f6e5bc17bb
listik111 <iafedorov05@icloud.com>
2024-12-02 13:28:08 +0300

    Add files via upload

---
+++ 1.3.py
@@ -0,0 +1,7 @@
+# Число n можно считать так:
+k = int(input())
+n = int(input())
+o = n // k
+k2 = n - o*k
+print(o)
+print(k2)
---
+++ 1.7.py
@@ -0,0 +1,5 @@
+n = int(input())
+m = int(input())
+p = int(input())
+k = (n + 1) // 2 + (m + 1) // 2 + (p + 1) // 2
+print(k)
