30d914719ab7e576857dd650e3c2930bda1aae0c
listik111 <iafedorov05@icloud.com>
2024-12-02 13:47:53 +0300

    Delete 1.6.py

--- 1.6.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-p = n - 1
-l = n + 1
-print('The next number for the number ' + str(n) + ' is ' + str(l) + '.')
-print('The previous number for the number ' + str(n) + ' is ' + str(p) + '.')
